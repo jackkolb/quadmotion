0283cdd1f08748ee39e550b475c8838fc19e9f43
Jack Kolb <jackhunterkolb@gmail.com>
2018-03-24 17:37:42 -0700

    more fleshing out, going to merge

--- serverlink/client/main.cpp
+++ serverlink/client/main.cpp
@@ -3,31 +3,31 @@
 #include <iostream>
 #include <boost/asio.hpp>
 #include <boost/array.hpp>
-#include <vector>
 
 using boost::asio::ip::tcp;
 using namespace std;
 
-int PACKET_SIZE = 64;
+const int PACKET_SIZE = 64;
 
 int main() {
-    try {
-        boost::asio::io_service ioservice;
-        tcp::resolver resolver(ioservice);
-        tcp::resolver::query query("127.0.0.1", "1234");
-        tcp::socket socket(ioservice);
+    cout << "-- CLIENT SERVICE --" << endl;
+    boost::asio::io_service ioservice;
+    tcp::resolver resolver(ioservice);
+    tcp::resolver::query query("127.0.0.1", "1234");
+    tcp::socket socket(ioservice);
+    while (true) {
+        try {
+            char signal;
+            cout << "enter signal: "; cin >> signal;
+            char output[PACKET_SIZE] = { signal };
 
-        char output[PACKET_SIZE] = { 'r' };
+            boost::asio::connect(socket, resolver.resolve(query)); // connect to server
+            boost::asio::write(socket, boost::asio::buffer(output, PACKET_SIZE)); // send packet
+        }
 
-        cout << "Press enter to send signal" << endl;
-        string trash; cin >> trash;
-
-        boost::asio::connect(socket, resolver.resolve(query));
-        boost::asio::write(socket, boost::asio::buffer(output, PACKET_SIZE));
-    }
-
-    catch (exception& e) {
-        cout << "EXCEPTION: " << e.what() << endl;
+        catch (exception& e) {
+            cout << "EXCEPTION: " << e.what() << endl;
+        }   
     }
 
     return 1;
--- serverlink/server/main.cpp
+++ serverlink/server/main.cpp
@@ -3,24 +3,18 @@
 #include <iostream>
 #include <boost/asio.hpp>
 #include <boost/optional.hpp>
-#include <vector>
 #include <string>
 
 using namespace std;
 using boost::asio::ip::tcp;
 
-const int PACKET_SIZE = 64;
-
-class Connection {
-    public:
-        void process_command(tcp::socket&, string);
-};
+const int PACKET_SIZE = 64; // packets will be 64 characters (although only 1 will be used)!
 
 int main() {
+    cout << "-- SERVER SERVICE --" << endl;
     while (true) { // makes the server go infinitely
         try {
             boost::asio::io_service ioservice; // initialize service
-            Connection session; // create session 
             int port = 1234; // port we are looking on
             tcp::acceptor acceptor (ioservice, tcp::endpoint(tcp::v4(), port)); // create acceptor mechanism
             tcp::socket socket(ioservice); // create socket
@@ -29,7 +23,9 @@ int main() {
 
             char input_buffer[PACKET_SIZE]; // this is the receiving buffer
             boost::asio::read(socket, boost::asio::buffer(input_buffer, 64)); // read data from the socket (blocks until buffer fills)
-            session.process_command(socket, input_buffer); // process the input
+            cout << "data received: ";
+            process_command(socket, input_buffer); // process the input
+            cout << endl;
         }
         catch (exception& e) {
             cerr << "EXCEPTION: " << e.what() << endl;
@@ -38,7 +34,8 @@ int main() {
     return 1;
 }
 
-void Connection::process_command(tcp::socket& socket, string a) {
-    cout << "DATA: " << a << endl;
+// PUT SERIAL IO STUFFS HERE!!
+void process_command(tcp::socket& socket, string a) {
+    cout << a;
     return;
 }
